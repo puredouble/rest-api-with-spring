0ecd31a1b740282829b394424c07a11fff14d8e9
puredouble <divaslive4@gmail.com>
2019-05-25 10:10:24 +0900

    errors resource create and test modify

---
+++ src/main/java/me/whiteship/restapiwithspring/index/IndexController.java
@@ -0,0 +1,18 @@
+package me.whiteship.restapiwithspring.index;
+
+import me.whiteship.restapiwithspring.events.EventController;
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
+@RestController
+public class IndexController {
+    @GetMapping("/api")
+    public ResourceSupport index() {
+        var index = new ResourceSupport();
+        index.add(linkTo(EventController.class).withRel("events"));
+        return index;
+    }
+}
